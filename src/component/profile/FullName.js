b3a467eb3c8f821221b8816cf696538039354659
name <name@gmail.com>
2021-12-18 13:53:52 +0100

    nada

---
+++ src/component/profile/FullName.js
@@ -0,0 +1,4 @@
+import React from "react";
+const Gretting =()=><h1>Hello Nada</h1>;
+export default Gretting;
+
---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,8 @@
+import React from "react";
+import myWonderfulImage from  './image1.png';
+const Gre =()=> 
+<img src={myWonderfulImage} alt ='imageInSrc' />
+
+
+export default Gre;
+
